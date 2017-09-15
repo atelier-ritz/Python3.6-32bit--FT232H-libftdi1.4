6ddc3dd27465b004cb237cf054fc30d470adb581
atelier-ritz <xutianqish@gmail.com>
2017-09-15 09:38:03 -0400

    DAC module finished

--- Example/[3]analog output/callbacks.py
+++ Example/[3]analog output/callbacks.py
@@ -4,36 +4,40 @@ from MCP4728 import MCP4728
 import math
 import time
 
+#=========================================================
+# a class that handles the signal and callbacks of the GUI
+#=========================================================
 # UI config
 qtCreatorFile = "mainwindow.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-
 # ft232h config
 FT232H.use_FT232H()
 ft232h = FT232H.FT232H()
-
 # MCP4728 (DAC) config
 i2c = MCP4728(ft232h, address=0x60, clock_hz=450000)
 
+#=========================================================
+# a class that handles the signal and callbacks of the GUI
+#=========================================================
 class GUI(QtWidgets.QMainWindow,Ui_MainWindow):
-
-    SIGGEN_SAMPLE_RATE = 16 # sample 16 points in each period, can be 2, 4, 8, 16, 32, etc.
-
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+        # signals and handlers
         self.sld_voltA.valueChanged.connect(self.on_sld_voltA)
         self.sld_voltB.valueChanged.connect(self.on_sld_voltB)
         self.sld_voltC.valueChanged.connect(self.on_sld_voltC)
         self.sld_voltD.valueChanged.connect(self.on_sld_voltD)
         self.chb_generateA.toggled.connect(self.on_chb_generateA)
 
+        # timer object
         self.my_timer = QtCore.QTimer()
-        self.my_timer.timeout.connect(self.updateSignalGenerator)
+        self.my_timer.timeout.connect(self.tmr_updateSiggen)
         self.siggenCounter = 0
 
+    # GUI callbacks
     def on_sld_voltA(self,val):
         i2c.setVoltage(0,val)
     def on_sld_voltB(self,val):
@@ -43,33 +47,36 @@ class GUI(QtWidgets.QMainWindow,Ui_MainWindow):
     def on_sld_voltD(self,val):
         i2c.setVoltage(3,val)
     def on_chb_generateA(self,boolean):
-        if boolean == True:
-            freq = self.dspb_freqA.value()
-            updateRate = (1 / freq) / (self.SINWAVE_SAMPLE_RATE) # sec
-            self.my_timer.start(updateRate*1000) # msec
+        if boolean:
+            self.my_timer.start(self._getTimerInterval())
         else:
             self.my_timer.stop()
 
-    def updateSignalGenerator(self):
-        waveform = self.cbb_waveformA.currentIndex()
+    # timer functions
+    def tmr_updateSiggen(self):
+        samplerate, freq, amp, offset = self._getSiggenParam()
+        self.my_timer.start(self._getTimerInterval())
+        self._siggenCounterInc()
+        val = int(math.sin(2*math.pi * self.siggenCounter/(samplerate+1)) * amp + offset)
+        i2c.setVoltage(0,val)
+
+    # private functions
+    def _getSiggenParam(self):
+        samplerate = self.spb_samplerate.value()
+        freq = self.dspb_freqA.value()
         amp = self.spb_ampA.value()
         offset = self.spb_offsetA.value()
-        freq = self.dspb_freqA.value()
-        samplerate = self.SIGGEN_SAMPLE_RATE
-        if not freq == 0:
-            updateRate = (1 / freq) / (self.SINWAVE_SAMPLE_RATE) # sec
-            self.my_timer.start(updateRate*1000)
-        if waveform == 1:    # sin
-            self.siggenCounter += 1
-            if self.siggenCounter >= samplerate:
-                self.siggenCounter = 0
-            val = int(math.sin(2 * math.pi * self.siggenCounter / (samplerate + 1))*amp+offset)
-            i2c.setVoltage(0,val)
-        if waveform == 2:   # square
-            self.siggenCounter += samplerate / 2
-            if self.siggenCounter >= samplerate:
-                self.siggenCounter = 0
-            val = int((-1) ** self.siggenCounter / (samplerate + 1))*amp+offset)
-            i2c.setVoltage(0,val)
-        if waveform == 3:   # triangle
-            #i2c.setVoltage(0,val)
+        return (samplerate,freq,amp,offset)
+    def _getTimerInterval(self):
+        samplerate, freq, _, _ = self._getSiggenParam()
+        if freq > 0:
+            updateRate = (1/freq) / samplerate * 1000
+            return updateRate # miliseconds
+        else:
+            print('Frequency must be positive! Timer interval is set to 1000 ms.')
+            return 1000
+    def _siggenCounterInc(self):
+        samplerate, _, _, _ = self._getSiggenParam()
+        self.siggenCounter += 1
+        if self.siggenCounter >= samplerate:
+            self.siggenCounter = 0
